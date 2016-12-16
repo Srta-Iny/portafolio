68bf74aa3f116e863c5eeba4734f12e6bb19bdb5
Srta-Iny <iny.mercado@gmail.com>
2016-12-16 13:29:06 -0300

    se realizan cambios recomendados

--- js/action.js
+++ js/action.js
@@ -1,4 +1,19 @@
     $(document).ready(function(){
     	$('.button-collapse').sideNav();
+
+    	/*$('#top').ScrollTo({
+		    duration: 2000,
+		    easing: 'linear'
+		});*/
+
+
+		//foto about 
+		/*$('#foto1').mouseover(function(){
+        	$('#foto1').addClass('hide');
+        	$('#foto2').removeClass('hide');
+    	});	
+		$('#foto1').mouseout(function(){
+        	$('#foto1').removeClass('hide');
+    	});*/
     });
-        
+    
